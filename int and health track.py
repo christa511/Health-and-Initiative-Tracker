ea454148449443831a11dcee0bee812a5ef81e3f
nbrownsw <nbrownsw@students.kennesaw.com>
2021-04-30 11:37:58 -0400

    fixed the naming, got the turn order functioning, and got it running smoothly

--- int and health track.py
+++ int and health track.py
@@ -1,4 +1,8 @@
-#NEED TO EXPLAIN WHAT THIS IS
+'''  
+This is code that will help a game master/dungeon master get the initiative and health of the players,
+put them all in correct initiative order, then help you go through the turns until combat is over.
+
+'''
 
 def validate_num_input(user_input):
     while True:
@@ -23,37 +27,46 @@ def get_initiative():
 def get_health(newinit):
     initiative = newinit
     health_tracking = {}
-    for player_name in initiative:
-        player_health = validate_num_input('What is {} health? '.format(player_name))
+    for item in initiative:
+        player_health = validate_num_input('What is {} health? '.format(initiative[item]))
         if player_health in health_tracking:
-            health_tracking[player_health].append(player_name)
+            health_tracking[player_health].append(item)
         else:
-            health_tracking[player_health] = [player_name]
+            health_tracking[player_health] = initiative[item]
     return(health_tracking)
 #code for sorting the list in numerical order
 def print_initiative(thisinit):
     print('\nYour initiative order is: ')
     for key in sorted(thisinit, reverse=True):
-        print('{}: {}'.format(key, ', '.join(thisinit[key])))
+        print(thisinit[key])
 
 if __name__ == '__main__':
     init_list = get_initiative()
     print_initiative(init_list)
+    print(init_list)
     print(get_health(init_list))
     
 
 while True:
-    for player in init_list:
-        print("It is", player, "'s turn.")
+    for key in sorted(init_list, reverse=True):
+        print("It is", init_list[key], "'s turn.")
         turn_action=input("press A to remove health, H to heal, S to skip turn, or L to leave combat. ")
+        #removes from players health
         if turn_action.lower() == "a":
             print("you attacked")
+            pass
+        #add to players health
         elif turn_action.lower() == "h":
             print("you healed")
+            pass
+        #skips the players turn
         elif turn_action.lower() == "s":
             print("you skipped your turn")
+            pass
+        #removes the player from the turn order
         elif turn_action.lower() == "l":
-            init_list.append(player)
+            init_list.pop(player)
+            break
     if init_list == "":
         break
     else:
